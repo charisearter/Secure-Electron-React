54a985c532ce1d974a9fde91d7ba302f3e57e59f
charisearter <58978773+charisearter@users.noreply.github.com>
2022-09-29 10:39:06 -0700

    Added notes to preload

--- preload.js
+++ preload.js
@@ -1,19 +1,22 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 const API = {
+	// Renderer to Main
 	send: (channel, data) => {
 		let validChannels = ['message', 'notify'];
 		if (validChannels.includes(channel)) {
 			ipcRenderer.send(channel, data);
 		}
 	},
+
+	// Bi-directional
 	invoke: (channel, data) => {
 		let validChannels = ['say-hello'];
 		if (validChannels.includes(channel)) {
 			ipcRenderer.invoke(channel, data);
 		}
 	},
-
+	// Main to Renderer
 	receive: (channel, func) => {
 		let validChannels = [];
 		if (validChannels.includes(channel)) {
