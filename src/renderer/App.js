39e7dc3fd4aa8052c10effe0175f88cbb6c3b695
charise-arter-sony <charisearter.dev@gmail.com>
2023-02-06 16:45:49 -0800

    Previous Path state persists & Doesn't delete when cancelled

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -5,26 +5,25 @@ import usePrevious from './hooks/usePrevious';
 function App() {
 	const [filePath, setFilePath] = useState([]);
 	const prevPathSrc = usePrevious(filePath);
-	const [prevSrc, setPrevSrc] = useState(prevPathSrc);
+	const [prevSrc, setPrevSrc] = useState('');
 
 	// options
 	let options = {
-		title: 'Testing Title',
 		defaultPath: filePath && filePath.length > 0 ? filePath : prevPathSrc,
-		properties: ['openFile', 'multiSelection'],
+		properties: ['openFile'],
 	};
 
 	// Show Dialog - Naive Open file
 	const fileOpen = async () => {
 		const thePath = await window.api.openNativeFile(options);
 		if (!thePath) {
-			setFilePath(prevPathSrc);
 			return;
 		}
 		console.log('Length of Path: ', thePath.length);
 		setFilePath(thePath);
-		setPrevSrc(thePath);
+		setPrevSrc(options.defaultPath);
 		console.log(`Render - default: ${options.defaultPath}`);
+		console.log(`Render - Prev PATH src: ${prevPathSrc}`);
 		console.log(`Render - Prev source: ${prevSrc}`);
 	};
 
